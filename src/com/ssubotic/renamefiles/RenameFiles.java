70b4fa332bd8eb7df11f1aa77e882e6bd3bb7498
Stefan Subotic <ssubotic@users.noreply.github.com>
2017-08-13 09:13:42 -0700

    Add JavaFX GUI and allow user input
    Refactor the main method to display a GUI created with JavaFX. This also
    allows for user input without the need to use a command line. The GUI
    includes a short description and screenshot showing how to get the file
    path of the directory that contains files in need of renaming. In the
    event of an invalid file path, no actions are taken and the program lets
    you know with an error message but does not terminate.

--- src/com/ssubotic/renamefiles/RenameFiles.java
+++ src/com/ssubotic/renamefiles/RenameFiles.java
@@ -1,19 +1,73 @@
 package com.ssubotic.renamefiles;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URL;
 
-public class RenameFiles 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class RenameFiles extends Application
 {   
-    
-    public static void main(String[] args) 
+    public void start(Stage stage)
     {
-        File dir = new File("C:\\Users\\Stefan\\Desktop\\2017-07-July\\2017-07-01");
-        File[] reports = dir.listFiles();
+        //pane set up
+        VBox primaryPane = new VBox(25);
         
-        for (File f : reports) {
-            renameFile(dir, f);
-            System.out.println(f);
-        }
+        Text text = new Text("This tool is for renaming nightly hotel reports. To use, copy and paste the current directory's complete filename.");
+        Image image = new Image("img.png", 750, 250, false, false);
+        ImageView imageView = new ImageView(image);
+        //use a horizontal box node so label and textField are on the same line
+        HBox inputPane = new HBox(10);
+        Label label = new Label("File Path: ");
+        TextField textField = new TextField();
+        Button button = new Button("Submit");
+        inputPane.getChildren().add(label);
+        inputPane.getChildren().add(textField);
+        inputPane.setAlignment(Pos.CENTER);
+        inputPane.getChildren().add(button);
+        
+        //add text, image, and inputPane to the primary pane
+        primaryPane.getChildren().add(text);
+        primaryPane.getChildren().add(imageView);
+        primaryPane.getChildren().add(inputPane);
+        primaryPane.setAlignment(Pos.CENTER);
+        primaryPane.setPadding(new Insets(10, 10, 10, 10));
+        
+        //associate pane with scene and scene with stage, set window size
+        Scene scene = new Scene(primaryPane, 800, 400);
+        stage.setTitle("Report Renamer");
+        stage.setScene(scene);
+        stage.show();
+        
+        button.setOnAction((event) -> {
+            try {
+                String input = textField.getText();
+                File dir = new File(input);
+                File[] reports = dir.listFiles();
+                for (File f : reports) {
+                    renameFile(dir, f);
+                    System.out.println(f);
+                }
+            } catch (Exception e) {
+                textField.setText("Error, invalid filepath");
+            }
+        });
+        
     }
     
     public static void renameFile(File dir, File f) 
